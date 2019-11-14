81ff9442feb7817028657c877f40b7b78e695a9a
Radek Ermel <radek.ermel@assurity.co.nz>
2019-11-14 14:32:28 +1300

    Initial commit, project started with Spring Initializr

---
+++ src/main/groovy/com/example/demo2/Demo2Application.groovy
@@ -0,0 +1,13 @@
+package com.example.demo2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Demo2Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Demo2Application, args)
+	}
+
+}
